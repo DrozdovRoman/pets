9fb6cb5ede61505ca17a5295da3dc1c164f4c224
Roman Drozdov <romandrozdov@icloud.com>
2023-06-09 15:26:15 +0500

    Create AnimalClass and Breed.

--- pets/api/models.py
+++ pets/api/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.db.models.constraints import UniqueConstraint
 
-# Create your models here.
+
+class AnimalClass(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name='Класс животного')
+    description = models.TextField(
+        verbose_name='Описание',
+    )
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Breed(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Порода')
+    animal_class = models.ForeignKey(AnimalClass, on_delete=models.CASCADE)
+    description = models.TextField(
+        verbose_name='Описание',
+    )
+    origin = models.CharField(max_length=100)
+    average_weight = models.PositiveIntegerField()
+    lifespan = models.PositiveIntegerField()
+    lifespan_unit = models.CharField(
+        max_length=20,
+    )
+    temperament = models.CharField(max_length=100)
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(
+                fields=('name', 'animal_class'),
+                name='Данная порода уже была добавлена',
+            )
+        ]
+        ordering = ('name', 'animal_class',)
+        verbose_name = 'Порода'
+        verbose_name_plural = 'Породы'
+
+    def __str__(self):
+        return self.name
