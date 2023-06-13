6b1a25d8b985e5049096e8d7558008418c097096
Roman Drozdov <romandrozdov@icloud.com>
2023-06-13 17:21:51 +0500

    Connect swagger.

--- pets/pets/urls.py
+++ pets/pets/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    # path("api/", include('api.urls', namespace='api'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += doc_urls
